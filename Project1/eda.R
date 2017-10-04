9d316bc9f0d7209d81c63f9974001cf8d7b14817
PedroArmengol <pedro.armengol.pedroza@gmail.com>
2017-10-04 12:47:58 -0500

    changes

--- Project1/eda.R
+++ Project1/eda.R
@@ -7,3 +7,12 @@ library("dplyr")
 library("tidyr")
 library("stringr")
 library("ggplot2")
+
+acc2014 <- read.csv("/Users/Usuario/Documents/Chicago/DataVis/Data_Visualization_Class/Project1/accident.csv")
+acc2015 <- read_sas("/Users/Usuario/Documents/Chicago/DataVis/Data_Visualization_Class/Project1/accident.sas7bdat")
+
+ls()
+
+class(acc2014)
+class(acc2015)
+
