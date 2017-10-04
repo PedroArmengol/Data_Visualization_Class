a2be8458f397d73f73c702fc5d637d702aba772d
PedroArmengol <pedro.armengol.pedroza@gmail.com>
2017-10-04 12:32:08 -0500

    EDA: R script

---
+++ Project1/eda.R
@@ -0,0 +1,9 @@
+# Pedro Armengol 
+# This is an exploratory analysis of the DoT FARS data
+
+library("readr")
+library("haven")
+library("dplyr")
+library("tidyr")
+library("stringr")
+library("ggplot2")
