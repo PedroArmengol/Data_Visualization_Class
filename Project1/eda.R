255f29f1af76785bcd5e677a8cb2f8713d815dfa
PedroArmengol <pedro.armengol.pedroza@gmail.com>
2017-10-04 14:32:14 -0500

    changes

--- Project1/eda.R
+++ Project1/eda.R
@@ -8,11 +8,25 @@ library("tidyr")
 library("stringr")
 library("ggplot2")
 
-acc2014 <- read.csv("/Users/Usuario/Documents/Chicago/DataVis/Data_Visualization_Class/Project1/accident.csv")
-acc2015 <- read_sas("/Users/Usuario/Documents/Chicago/DataVis/Data_Visualization_Class/Project1/accident.sas7bdat")
-
+acc2015 <- read.csv("/Users/Usuario/Documents/Chicago/DataVis/Data_Visualization_Class/Project1/accident.csv")
+acc2014 <- read_sas("/Users/Usuario/Documents/Chicago/DataVis/Data_Visualization_Class/Project1/accident.sas7bdat")
+#See enviroment
 ls()
-
+#Check class of the object
 class(acc2014)
 class(acc2015)
+
+# Impute missing values
+acc2014 <- mutate(acc2014, TWAY_ID2 = na_if(TWAY_ID2,""))
+table(is.na(acc2014$TWAY_ID2))
+
+dim(acc2014)
+dim(acc2015)
+
+
+elem1 <-  colnames(acc2014)[colnames(acc2014) %in% colnames(acc2015) == FALSE]
+elem1
+elem2 <- colnames(acc2015)[colnames(acc2015) %in% colnames(acc2014) == FALSE]
+elem2
+
 
