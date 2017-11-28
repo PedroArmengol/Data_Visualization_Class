5b1aef2e84813b501400be47c8bf3ad30cf9cb81
Pedro Armengol <pedro@MacBook-Pro-de-Pedro.local>
2017-11-28 14:34:48 -0800

    Load data (csv)

--- ATF_D3/D3/script.js
+++ ATF_D3/D3/script.js
@@ -3,5 +3,24 @@ var width = 1100 - margin.left - margin.right;
 var height = 800 - margin.top - margin.bottom;
 var barPadding = 5;
 
+//Import the data
 
+
+d3.csv("merchants_state.csv", function(error, data){
+  if (error) { 
+    console.log(error);
+  } else {
+    data.forEach(function(d){
+      d.lon = +d.lon,
+      d.lat = +d.lat,
+      d.year = +d.year,
+      d.type = +d.type,
+      d.name = d.license_name
+    });
+    dataset = data;
+    console.log(dataset);
+    //createChart(dataset);
+
+  };
+});
 
