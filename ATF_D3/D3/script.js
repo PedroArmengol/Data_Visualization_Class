1977f3077c09786eb2d1e3f6c39d0919ad4bdd0e
Pedro Armengol <pedro@MacBook-Pro-de-Pedro.local>
2017-11-28 23:50:05 -0600

    addition of svg, points,scales,axis and axis labels

--- ATF_D3/D3/script.js
+++ ATF_D3/D3/script.js
@@ -19,8 +19,97 @@ d3.csv("merchants_state.csv", function(error, data){
     });
     dataset = data;
     console.log(dataset);
-    //createChart(dataset);
+    createChart(dataset);
 
   };
 });
 
+var svg = d3.select("body")
+              .append("svg")
+              .attr("height", height + margin.top + margin.bottom)
+              .attr("width", width + margin.left + margin.right);
+//Graph
+function createChart(d){
+
+  //Y Scale (continuous)
+    var yScale = d3.scaleLinear()
+                   .domain([d3.min(dataset, function (d){return d.lat;}), d3.max(dataset, function (d){return d.lat;})])
+                   .range([height,0]); 
+
+    //X scale (categorical)
+    var xScale = d3.scaleLinear()
+                   .domain([d3.min(dataset, function (d){return d.lon;}), d3.max(dataset, function (d){return d.lon;})])
+                   .range([0, width])
+
+ //Draw the scatter plot
+  svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) {
+      return xScale(d.lon);
+    })
+    .attr("cy",function(d){
+      return yScale(d.lat);
+    })
+    .attr("r", 5)
+    .attr("class", function(d){
+
+          if (d.year == 2014){
+
+            return "point_2014";
+
+          if (d.year == 2015)
+
+            return "point_2015";
+
+          if (d.year == 2016)
+
+            return "point_2016";
+
+          } else {
+
+            return "point_2017";
+
+            }
+          
+        });
+
+    //Y axis
+    var yAxis = d3.axisLeft()
+                  .scale(yScale)
+                  .ticks(10);
+
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + (margin.left-barPadding*2) + "," + margin.top + ")")
+        .call(yAxis);
+
+    //Y axis label
+    svg.append("text")
+        .attr("class", "axis_label")
+        .text("Latitude")
+        .attr("transform", "translate("+ margin.left/3 +"," + (margin.top+height/2) + ") rotate(270)")
+        .attr("text-anchor", "middle");
+
+    //X axis
+    var xAxis = d3.axisBottom()
+                  .scale(xScale)
+                  .ticks(10);
+ 
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + margin.left + "," + (margin.top+height+barPadding*2) + ")")
+        .call(xAxis);
+
+    //X axis label
+    svg.append("text")
+        .attr("class", "axis_label")
+        .text("Longitude")
+        .attr("transform", "translate("+ (margin.left + width/2) +"," + (margin.top+height+barPadding*12) + ")")
+        .attr("text-anchor", "middle");
+
+
+  
+   
+  };
